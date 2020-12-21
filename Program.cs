f972d910b096687eb2928f2021b5693ba97e4bd2
Obi Oberoi <obi@eoberoi.com>
2020-12-21 10:50:47 -0500

    added comment from surface

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace InterviewQuestions
     class Program
     {
         //will start using it more frequently!!!
+        //this is pretty cool stuff. Just pulled the files from git on my surface
         static void Main(string[] args)
         {
             //Reference Type
