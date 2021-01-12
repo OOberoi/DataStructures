690d6c4e32bafa93595a28f24ae3c50633e6cdce
Obi Oberoi <obi@eOberoi.com>
2021-01-11 20:55:37 -0500

    minor change

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace InterviewQuestions
 
             //FindNumber Starts here
             bool isTrue = Linq.FindNumber(13);
-            Console.WriteLine(isTrue == true ? "Number matches!" : "Number does not match!");
+            Console.WriteLine(isTrue == true ? "Number matches" : "Number does not match!");
             Console.ReadLine();
             // Ends here
 
