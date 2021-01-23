69270393a2dc2f618920ddbe0731f2159499896d
Obi Oberoi <obi@eOberoi.com>
2021-01-22 19:02:02 -0500

    added person class

---
+++ Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    public class Person
+    {
+        public string Name { get; }
+        public string Age { get; }
+    }
+
+    
+}
