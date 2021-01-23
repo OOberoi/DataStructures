c67e36f80ad80a709dccd301f0c6373ad3a1fdad
Obi Oberoi <obi@eOberoi.com>
2021-01-22 19:05:52 -0500

    added a new data member

--- Person.cs
+++ Person.cs
@@ -6,9 +6,8 @@ namespace Arrays
 {
     public class Person
     {
-        public string Name { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
         public string Age { get; }
     }
-
-    
 }
