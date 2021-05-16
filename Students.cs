6cd6cb8460b7c51633da41da9fcca59cc58113e9
Obi Oberoi <obi@eOberoi.com>
2021-05-16 19:51:52 -0400

    changed data type

--- Students.cs
+++ Students.cs
@@ -9,6 +9,7 @@ namespace Arrays
         public int StudentID { get; set; }
         public string StudentName { get; set; }
         public int Age { get; set; }
+        public char Gender { get; set; }
     }
 
 
