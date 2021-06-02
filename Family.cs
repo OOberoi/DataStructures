865303f08776282963a612112b887666c02fc048
Obi Oberoi <obi@eOberoi.com>
2021-06-01 22:00:56 -0400

    built the family class

--- Family.cs
+++ Family.cs
@@ -4,5 +4,20 @@ namespace Models
 {
     public class Family
     {
+        public string FamilyName { get; set; }
+        public string AddressLine { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string FatherName { get; set; }
+        public string MotherName { get; set; }
+        public string FirstChild { get; set; }
+        public string SecondChild { get; set; }
+        public string ThirdChild { get; set; }
+        public string FourthChild { get; set; }
+        public string FifthChild { get; set; }
+        public string FirstPet { get; set; }
+        public string SecondPet { get; set; }
+        public string ThirdPet { get; set; }
     }
 }
