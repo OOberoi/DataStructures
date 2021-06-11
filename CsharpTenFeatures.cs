0fcf1709dff2ae297cf9e35334e569fcf4b2cc03
Obi Oberoi <obi@eOberoi.com>
2021-06-10 22:20:54 -0400

    added a record

--- CsharpTenFeatures.cs
+++ CsharpTenFeatures.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,14 @@ namespace Arrays
 {
     public class CsharpTenFeatures
     {
+        record Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public void WritToFile(string filePath)
+                => File.WriteAllText(filePath, ToString());
+        }
+
     }
 }
