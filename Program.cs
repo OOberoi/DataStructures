b14fa91cd812ebe170ffd44cdf4ae0d6a88f523d
Obi Oberoi <obi@eOberoi.com>
2021-06-28 21:33:04 -0400

    minor changes

--- Program.cs
+++ Program.cs
@@ -143,9 +143,9 @@ namespace InterviewQuestions
             //Anonymous Types - IList does not provide Sort even though it is higher in hierarchy
             IList<Students> studentList = new List<Students>()
             {
-                new Students() { StudentID = 1, StudentName = "John Oliver", Age = 18, Gender = 'M' },
-                new Students() { StudentID = 2, StudentName = "Steve Bassett",  Age = 21, Gender = 'M' },
-                new Students() { StudentID = 3, StudentName = "Bill Gates",  Age = 23, Gender = 'M'},
+                new Students() { StudentID = 1, StudentName = "John Oliver", Age = 78, Gender = 'M' },
+                new Students() { StudentID = 2, StudentName = "Steve Bassett",  Age = 51, Gender = 'M' },
+                new Students() { StudentID = 3, StudentName = "Bill Gates",  Age = 63, Gender = 'M'},
                 new Students() { StudentID = 4, StudentName = "Ram Vilas" , Age = 25, Gender = 'M'},
                 new Students() { StudentID = 5, StudentName = "Ron Kramer" , Age = 27, Gender = 'M' },
                 new Students() { StudentID = 6, StudentName = "Obi Oberoi" , Age = 45, Gender = 'M'},
