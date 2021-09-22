78775c0de6bb12b697d3c8b51ea41db2fcc467d8
Obi Oberoi <obi@eoberoi.com>
2021-09-21 23:20:54 -0400

    declared a bunch of dynamic variables

--- Dynamic.cs
+++ Dynamic.cs
@@ -9,8 +9,11 @@ namespace Arrays
     public static class DynamicNew
     {
         private static void EvalDynamicValues()
-        { 
-        
+        {
+            dynamic myVal = 100;
+            dynamic myStr = "Hello Obi";
+            dynamic mySalary = "100000.00";
+            dynamic myFlag = true;
         }
 
     }
