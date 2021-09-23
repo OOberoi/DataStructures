68237bc0a950e5a4c58e45b1dff92248e1393709
Obi Oberoi <obi@eoberoi.com>
2021-09-22 22:52:17 -0400

    wrote the type to console

--- Dynamic.cs
+++ Dynamic.cs
@@ -20,6 +20,7 @@ namespace Arrays
             Console.WriteLine($"The type of {myStr} is: {myStr.GetType()}");
             Console.WriteLine($"The type of {mySalary} is: {mySalary.GetType()}");
             Console.WriteLine($"The type of {myFlag} is: {myFlag.GetType()}");
+            Console.WriteLine($"The type of {currDate} is: {currDate.GetType()}");
         }
 
     }
